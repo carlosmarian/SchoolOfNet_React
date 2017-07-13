193fe99eb8261c3a0122e3cc2939f273d639aa5b
Desenvolvedor <desenvolvedor@girosistemas.com.br>
2017-07-13 17:33:13 -0300

    Adicionando itens a lista.

--- course/src/components.js
+++ course/src/components.js
@@ -54,23 +54,67 @@ var Button = React.createClass({
 });
 
 var Form = React.createClass({
+    getInitialState: function () {
+        return {
+            name: '',
+            email: '',
+            subject: 'R',
+            messenger: ''
+        }
+    },
+    handleNameChange: function (e) {
+        this.setState({
+            name: e.target.value
+        });
+        console.log('sss');
+    },
+    handleEmailChange: function (e) {
+        this.setState({
+            email: e.target.value
+        });
+    },
+    handleSubjectChange: function (e) {
+        this.setState({
+            subject: e.target.value
+        });
+    },
+    handleMessengerChange: function (e) {
+        this.setState({
+            messenger: e.target.value
+        });
+    },
+
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var name = this.state.name.trim();
+        var email = this.state.email.trim();
+        var subject = this.state.subject;
+        var messenger = this.state.messenger.trim();
+
+        //Verifica se algum não foi informado
+        if (!name || !email || !subject || !messenger) {
+            return;
+        }
+
+        this.props.onContactSubmit({ id: this.props.idNumber, name: name, email: email, subject: subject, messenger: messenger });
+    },
     render: function () {
         var inputStyle = {
             color: "#A7A5A5"
         };
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" id="name" style={inputStyle} placeholder="Name" />
+                    <input type="text" className="form-control" id="name" onChange={this.handleNameChange} style={inputStyle} placeholder="Name" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">E-mail</label>
-                    <input style={inputStyle} type="email" className="form-control" id="email" placeholder="E-mail" />
+                    <input style={inputStyle} type="email" className="form-control" id="email" onChange={this.handleEmailChange} placeholder="E-mail" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="subject">Subject</label>
-                    <select defaultValue="R" className="form-control" id="subject" >
+                    <select defaultValue={this.state.subject} className="form-control" id="subject" onChange={this.handleSubjectChange} >
                         <option value="A">Angular</option>
                         <option value="V">VueJS</option>
                         <option value="R">React</option>
@@ -78,8 +122,9 @@ var Form = React.createClass({
                 </div>
                 <div className="form-group">
                     <label htmlFor="messenger">Messenger</label>
-                    <textarea style={inputStyle} type="email" className="form-control" id="messenger" rows="3"></textarea>
+                    <textarea style={inputStyle} type="email" className="form-control" id="messenger" onChange={this.handleMessengerChange} rows="3"></textarea>
                 </div>
+                <Button textActive="Loading..." >Send</Button>
             </form>
         );
     }
--- course/src/main.js
+++ course/src/main.js
@@ -1,32 +1,40 @@
-var data = [
-    { id: "1", name: "Maria", email: "etste@tstes", subject: "R", messenger: "Um teste maior react" },
-    { id: "2", name: "Pedro", email: "ee2@tstes", subject: "A", messenger: "Um teste maio angular" }
-];
 
-var Page = React.createClass(
-    {
-        render: function () {
-            return (
-                <myElement>
-                    <Nav title="Props React" linkUrl="index.html" />
-                    <div className="container">
-                        <Title fontSize="50px">
-                            My title component!
+var Page = React.createClass({
+    getInitialState: function () {
+        return {
+            data: [
+                { id: "1", name: "Maria", email: "etste@tstes", subject: "R", messenger: "Um teste maior react" },
+                { id: "2", name: "Pedro", email: "ee2@tstes", subject: "A", messenger: "Um teste maio angular" }
+            ]
+        };
+    },
+    handleContactSubmit: function (contact) {
+        console.log(contact);
+        var newContacts = this.state.data.concat([contact]);
+        this.setState({
+            data: newContacts
+        });
+    },
+    render: function () {
+        return (
+            <myElement>
+                <Nav title="Props React" linkUrl="index.html" />
+                <div className="container">
+                    <Title fontSize="50px">
+                        My title component!
                         </Title>
-                        <div className="row">
-                            <Form />
-                            <Button textActive="Loading..." >Send</Button>
-                        </div>
-                        <div className="row">
-                            <List data={data} />
-                            <br /><br />
-                        </div>
+                    <div className="row">
+                        <Form onContactSubmit={this.handleContactSubmit} idNumber={this.state.data.length + 1} />
                     </div>
-                </myElement>
-            )
-        }
+                    <div className="row">
+                        <List data={this.state.data} />
+                        <br /><br />
+                    </div>
+                </div>
+            </myElement>
+        )
     }
-);
+});
 
 ReactDOM.render(
     <Page />,
